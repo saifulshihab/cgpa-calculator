21103630e0b0c87701fb4d1eed6e6f38ecf4013a
SaifulShihab <shihabmd1970@gmail.com>
2020-05-01 19:49:44 +0600

    ES6

--- calculator.js
+++ calculator.js
@@ -1,85 +1,71 @@
-$(document).ready(function() {
+const calculate_btn = document.getElementById("cal-btn");
+
+$(document).ready(function () {
   $("#checkb").hide();
-  var e;
-  $("#semester_select").change(function() {
-    e = $("#semester_select").val();
-    createForm(e);
+  $("#semester_select").change(function () {
+    const nof_semester = $("#semester_select").val();
+    createForm(nof_semester);
     $("#checkb").show();
   });
 });
 
-function createForm(m) {
+const createForm = (s) => {
   document.getElementById("calform").innerHTML = "";
-  var i;
-  var e;
-  for (i = 0; i < m; i++) {
-    var x = document.createElement("input");
+  let i;
+  for (i = 0; i < s; i++) {
+    const x = document.createElement("input");
     x.type = "text";
-    x.id = "sgpa";
-    x.name = "sgpa";
     x.placeholder = "SGPA " + (i + 1) + "";
-    x.setAttribute("class", "inputf");
+    x.setAttribute("class", "sgpa");
     x.classList.add("mb-1");
-    x.classList.add("sgpa");
 
-    var y = document.createElement("input");
+    const y = document.createElement("input");
     y.type = "text";
-    y.name = "credit";
-    y.id = "credit";
     y.placeholder = "Credit " + (i + 1);
-    y.setAttribute("class", "inputf");
+    y.setAttribute("class", "credit");
     y.classList.add("mb-1");
-    y.classList.add("credit");
 
-    e = document.getElementById("calform");
+    const e = document.getElementById("calform");
     e.appendChild(x);
     e.appendChild(y);
   }
-  var chk = document.createElement("input");
-  chk.type = "checkbox";
-  chk.setAttribute("data-toggle", "toggle");
-}
-function cgpaCalculate() {
-  var sgpa = document.getElementsByName("sgpa");
-  var credit = document.getElementsByName("credit");
-  var result1 = 0,
+};
+const cgpaCalculate = () => {
+  const sgpa = document.querySelectorAll(".sgpa");
+  const credit = document.querySelectorAll(".credit");
+  let result1 = 0,
     result2 = 0;
-  for (var i = 0; i < sgpa.length; i++) {
+  for (let i = 0; i < sgpa.length; i++) {
     if (sgpa[i].value == "" || credit[i].value == "") {
       alert("Fill all input box.");
-      break;
+      return;
     } else if (sgpa[i].value == "" && credit[i].value == "") {
       alert("Fill all input box.");
-      break;
+      return;
+    } else if (!+sgpa[i].value || !+credit[i].value) {
+      alert("Invalid input!");
+      return;
     } else {
       result1 += parseFloat(sgpa[i].value) * parseFloat(credit[i].value);
       result2 += parseFloat(credit[i].value);
       document.getElementById("result").innerHTML =
-    "Your CGPA : " + parseFloat(result1 / result2).toFixed(2);
-    }
-  }  
-}
-
-function btn() {
-  var credit = document.getElementsByName("credit");
-  var cdit = ["11", "13", "13", "12", "14", "13", "13", "13"];
-  for (var i = 0; i < credit.length; i++) {
-    credit[i].value = cdit[i];
-    console.log(cdit[i]);
-  }
-  alert("sdsdsd");
-}
-
-document.getElementById("diustd").onclick = function() {
-  let credit = document.getElementsByName("credit");
-  let cdit = ["11", "13", "13", "12", "14", "13", "13", "13", "14", "14", "11"];
-  if ( this.checked ) {
-    for (let i = 0; i < credit.length; i++) {
-      credit[i].value = cdit[i];     
-    }
-  } else {
-    for (let i = 0; i < credit.length; i++) {
-      credit[i].value = '';     
+        "Your CGPA : " + parseFloat(result1 / result2).toFixed(2);
     }
   }
 };
+
+document.getElementById("diustd").onclick = function () {
+  let credit = document.querySelectorAll(".credit");
+  let cdit = ["11", "13", "13", "12", "14", "13", "13", "13", "14", "14", "11"];
+  if (this.checked) {
+    for (let i = 0; i < credit.length; i++) {
+      credit[i].value = cdit[i];
+    }
+  } else {
+    for (let i = 0; i < credit.length; i++) {
+      credit[i].value = "";
+    }
+  }
+};
+
+calculate_btn.addEventListener("click", cgpaCalculate);
